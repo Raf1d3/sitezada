165de791da0af9a877410940962a49c7460a3035
Raf1d3 <pmrafa9@gmail.com>
2023-10-05 19:22:13 -0300

    alterações js

--- arquivos/js/js.js
+++ arquivos/js/js.js
@@ -1,33 +1,32 @@
-
 function mudou(){
-document.getElementById("ordem").innerHTML += "<br> Paulo da cu";
-
-
+  document.getElementById("ordem").innerHTML += "<br> Paulo da cu";
 if(bola.style.display === 'none'){
   bola.style.display = 'block';
 }
 else{
   bola.style.display = 'none';
 }
-
 }
 
 
-// faz aparecer e desaperecer gif do pato
-document.addEventListener("DOMContentLoaded", function() {
-    const bot_rdp = document.getElementById('bot_rdp');
-    const duck_flip = document.getElementById('duck_flip');
+    // Define o estado inicial do gif para 'none'
+function carregou(){  
+    duck_flip.style.display = 'none';
+}  
+function oneclick(){
+  document.getElementById("duck_flip").src="arquivos/imagem/pato-girando.gif";
+    // Verifica se a imagem está visível ou escondida
 
-// Adiciona um evento de clique ao botão
-bot_rdp.addEventListener('click', () => {
-  // Verifica se a imagem está visível ou escondida
-  if (duck_flip.style.display === 'none') {
+    if (duck_flip.style.display === 'none') {
     // Se estiver escondida, torna-a visível (display: block)
     duck_flip.style.display = 'block';
   } else {
     // Se estiver visível, torna-a escondida novamente (display: none)
     duck_flip.style.display = 'none';
-     }
-    });
+  }
+}
 
-});
+function twoclick(){
+  document.getElementById("duck_flip").src="arquivos/imagem/segredo.jpg";
+
+}
